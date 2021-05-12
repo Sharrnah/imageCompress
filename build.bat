0696ff885979dac3ddf59fe839958f5aa70aa434
Sharrnah <alina4@gmail.com>
2021-05-12 21:02:27 +0200

    Add file icon

--- build.bat
+++ build.bat
@@ -3,6 +3,11 @@ set GOOS=windows
 set CGO_ENABLED=1
 set GOTMPDIR=%cd%
 
-go build -tags osusergo -v -ldflags "-s -w" -o bin/imageResize.exe
+set origdir=%cd%
 
-echo go build -tags osusergo -v -compiler=gccgo -gccgoflags "-s -w" -o bin/imageResize_gccgo.exe
+cd bin
+windres -o imageCompress-res.syso imageCompress.rc
+cd %origdir%
+go build -tags osusergo -v -ldflags "-s -w" -o bin/imageCompress.exe
+
+echo go build -tags osusergo -v -compiler=gccgo -gccgoflags "-s -w" -o bin/imageCompress_gccgo.exe
